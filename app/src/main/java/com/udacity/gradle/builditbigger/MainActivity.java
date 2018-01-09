65afd1e27dfb53998ae15076cc9d56693166aa36
Martins Grunskis <martins@grunskis.com>
2018-01-09 20:17:22 +0100

    setup gce (step 3)

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import com.grunskis.jokes.Jokes;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.grunskis.jokesactivity.JokeActivity;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,9 +51,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        String joke = Jokes.getRandomJoke();
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(JokeActivity.EXTRA_JOKE, joke);
-        startActivity(intent);
+        final Context context = this;
+
+        new AsyncTask<Void, Void, String>() {
+            private MyApi api = null;
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                if (api == null) {
+                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                            new AndroidJsonFactory(), null)
+                            // options for running against local devappserver
+                            // - 10.0.2.2 is localhost's IP address in Android emulator
+                            // - turn off compression when running against local devappserver
+                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                                @Override
+                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                    abstractGoogleClientRequest.setDisableGZipContent(true);
+                                }
+                            });
+
+                    api = builder.build();
+                }
+                try {
+                    return api.getJoke().execute().getJoke();
+                } catch (IOException e) {
+                    return e.getMessage();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String joke) {
+                Intent intent = new Intent(context, JokeActivity.class);
+                intent.putExtra(JokeActivity.EXTRA_JOKE, joke);
+                startActivity(intent);
+            }
+        }.execute();
     }
 }
