4956db3ed22fc0bfd81f6ba5b63c03bca4e60c7f
Martins Grunskis <martins@grunskis.com>
2018-01-09 11:49:59 +0100

    replace jokes with real ones

--- jokes/src/main/java/com/grunskis/jokes/Jokes.java
+++ jokes/src/main/java/com/grunskis/jokes/Jokes.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class Jokes {
 
     private static final String[] JOKES = {
-            "Joke 1",
-            "Joke 2",
-            "Joke 3",
-            "Joke 4",
-            "Joke 5"
+            // taken from http://www.gotlines.com/jokes/
+            "Just changed my Facebook name to 'No one' so when I see stupid posts I can click like and it will say 'No one likes this'.",
+            "What's the difference between snowmen and snowladies? Snowballs",
+            "How do you make holy water? You boil the hell out of it.",
+            "Why did the blonde get excited after finishing her puzzle in 6 months? -- The box said 2-4 years!",
+            "What do you call a fat psychic? A four chin teller."
     };
 
     public static String getRandomJoke() {
